91bed0a56e820d02360e02e561a85d8c44c60527
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-20 22:11:08 -0500

    add some margin to nav title

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,7 @@ import rotatingliner from "./images/Picture1.jpg"
 
 const config: DocsThemeConfig = {
   // gitTimestamp: true,
-  logo: <span className={"nx-flex nx-items-center"}><Image src={rotatingliner} alt={"rotating liner"} width={35} height={35} priority className={"nx-w-8 nx-h-8"}/>The Rotating Liner Engine</span>,
+  logo: <span className={"nx-flex nx-items-center"}><Image src={rotatingliner} alt={"rotating liner"} width={35} height={35} priority className={"nx-w-8 nx-h-8 nx-mr-2"}/>The Rotating Liner Engine</span>,
   footer: {
     text: `Tribo Dynamics Inc.`,
   },
