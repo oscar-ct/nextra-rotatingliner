2838b63c5b3a9953e2011347bfb02f718389a9f7
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-20 22:03:38 -0500

    complete rotating liner app using nextra

---
+++ theme.config.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from "next/image";
+import rotatingliner from "./images/Picture1.jpg"
+
+const config: DocsThemeConfig = {
+  // gitTimestamp: true,
+  logo: <span className={"nx-flex nx-items-center"}><Image src={rotatingliner} alt={"rotating liner"} width={35} height={35} priority className={"nx-w-8 nx-h-8"}/>The Rotating Liner Engine</span>,
+  footer: {
+    text: `Tribo Dynamics Inc.`,
+  },
+  feedback: {
+    useLink: args => "mailto:oscar.a.castro818@gmail.com"
+  },
+  editLink: {
+    component: null
+  },
+}
+
+export default config
