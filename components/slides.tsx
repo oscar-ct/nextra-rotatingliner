e4620aec9d001a9e7495654ed37ee8d040f54fcf
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-28 19:25:27 -0500

    replace emoji with icon, adjust formatting

--- components/slides.tsx
+++ components/slides.tsx
@@ -13,6 +13,8 @@ import img4 from "../images/80_Hr_Inspection_04.webp";
 import img5 from "../images/80_Hr_Inspection_05.webp";
 import img6 from "../images/80_Hr_Inspection_06.webp";
 import {useCallback, useEffect, useRef, useState} from "react";
+import { GoDotFill } from "react-icons/go";
+
 
 const Slides = () => {
     const swiperRef = useRef(null);
@@ -34,6 +36,7 @@ const Slides = () => {
         };
     }, [updateIndex]);
 
+    const bulletSize : number = 24;
 
     return (
         <>
@@ -73,37 +76,54 @@ const Slides = () => {
             </Swiper>
             {
                 currentSlide === 0 && (
-                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-text-xl nx-gap-4"}>
-                        <div>
-                            <span className={"nx-text-sm"}>&#128308;</span> - Thrust bearing inserts plus pre-loading o-rings.
+                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-gap-4 nx-text-lg"}>
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"red"}/>
+                            </div>
+                            Thrust bearing inserts plus pre-loading o-rings.
                         </div>
-                        <div>
-                        <span className={"nx-text-sm"}>&#128309;</span> - Grooves on head gasket that relieve pressure behind secondary seal during startup, allowing seal
-                            to seat. Prior to this, primary sealing ring leaked occasionally.
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"blue"}/>
+                            </div>
+                            Grooves on head gasket that relieve pressure behind secondary seal during startup, allowing seal to seat. Prior to this, primary sealing ring leaked occasionally.
                         </div>
-                        <div>
-                        <span className={"nx-text-sm"}>&#128994;</span> - Area on RL face that engages head insert thrust bearing brass inserts.
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"limegreen"}/>
+                            </div>
+                            Area on RL face that engages head insert thrust bearing brass inserts.
                         </div>
                     </div>
                 )
             }
             {
                 currentSlide === 1 && (
-                    <div className={"nx-flex nx-flex-col nx-text-xl nx-gap-4"}>
-                        <div className={"nx-inline-block"}>
-                            <span className={"nx-text-sm"}>&#128308;</span> - Carbon deposits on head shield OD stops here,
+                    <div className={"nx-flex nx-flex-col nx-gap-4 nx-text-lg"}>
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"red"}/>
+                            </div>
+                            Carbon deposits on head shield OD stops here,
                             indicating no blowby flow.
                         </div>
-                        <div className={"nx-inline-block"}>
-                            <span className={"nx-text-sm"}>&#128309;</span> - Springs are clean from carbon deposits.
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"blue"}/>
+                            </div>
+                            Springs are clean from carbon deposits.
                         </div>
                     </div>
                 )
             }
             {
                 currentSlide === 2 && (
-                    <div className={"nx-text-xl nx-pt-8"}>
-                        <span className={"nx-text-sm"}>&#128308;</span> - 0.040" O-ring that preloads thrust inserts in order to deal with RL face runout. Brass thrust
+                    <div className={"nx-flex nx-items-center nx-gap-2 nx-pt-8 nx-text-lg"}>
+                        <div style={{width: bulletSize}}>
+                            <GoDotFill size={bulletSize} fill={"red"}/>
+                        </div>
+                        0.040" O-ring that preloads thrust inserts in order to deal with RL face runout. Brass thrust
                         bearing inserts take upwards thrust load from oil pressure. Rubbing faces show only minor wear,
                         probably just break-in, faces polished.
                     </div>
@@ -111,33 +131,45 @@ const Slides = () => {
             }
             {
                 currentSlide === 3 && (
-                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-text-xl nx-gap-4"}>
-                        <div>
-                            <span className={"nx-text-sm"}>&#128308;</span> - The source of these scratches is unknown, they likely originated during assembly. Scratches are
-                            too shallow to interfere with sealing.
+                    <div className={"nx-flex nx-items-center nx-gap-2 nx-pt-8 nx-text-lg"}>
+                        <div style={{width: bulletSize}}>
+                            <GoDotFill size={bulletSize} fill={"red"}/>
                         </div>
+                        The source of these scratches is unknown, they likely originated during assembly. Scratches are too shallow to interfere with sealing.
                     </div>
+
                 )
             }
             {
                 currentSlide === 4 && (
-                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-text-xl nx-gap-4"}>
-                        <div>
-                            <span className={"nx-text-sm"}>&#128308;</span> - Carbon deposits stop here, shiny above that spot. Again, no blowby flow.
+                    <div className={"nx-flex nx-items-center nx-gap-2 nx-pt-8 nx-text-lg"}>
+                        <div style={{width: bulletSize}}>
+                            <GoDotFill size={bulletSize} fill={"red"}/>
                         </div>
+                        Carbon deposits stop here, shiny above that spot. Again, no blowby flow.
                     </div>
                 )
             }
             {
                 currentSlide === 5 && (
-                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-text-xl nx-gap-4"}>
-                        <div>
-                            <span className={"nx-text-sm"}>&#128308;</span> - This area on OD of RL, about 0.100 wide showed distress in prior runs, now with expanded oil
-                            groove no distress is visible, but we still think this area still causes thermal seizure due to
+                    <div className={"nx-flex nx-flex-col nx-pt-8 nx-gap-4 nx-text-lg"}>
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"red"}/>
+                            </div>
+                            This area on OD of RL, about 0.100 wide
+                            showed
+                            distress in prior runs, now with expanded oil
+                            groove no distress is visible, but we still think this area still causes thermal seizure due
+                            to
                             still inadequate oil supply.
                         </div>
-                        <div>
-                            <span className={"nx-text-sm"}>&#128309;</span> - Brass thrust washer, takes downward thrust load from springs until oil pressure builds up, face
+                        <div className={"nx-flex nx-items-center nx-gap-2"}>
+                            <div style={{width: bulletSize}}>
+                                <GoDotFill size={bulletSize} fill={"blue"}/>
+                            </div>
+                            Brass thrust washer, takes downward thrust
+                            load from springs until oil pressure builds up, face
                             shiny, very little wear.
                         </div>
                     </div>
